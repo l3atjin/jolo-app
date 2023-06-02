830a2fe32a1973af795aa769b495bef9fa354789
Batjin Lamjav <batjinlamjav@Batjins-MacBook-Pro-3.local>
2023-06-01 20:32:26 -0700

    created expo app and connected it to supabase

---
+++ jolo-app/lib/supabase.tsx
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto'
+import * as SecureStore from 'expo-secure-store'
+import { createClient } from '@supabase/supabase-js'
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key)
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value)
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key)
+  },
+}
+
+const supabaseUrl = "http://localhost:54321"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
