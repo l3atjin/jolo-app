f6ef6a6a84362ea4d433a2a3cd05868b01368b19
Batjin Lamjav <batjinlamjav@Batjins-MacBook-Pro-3.local>
2023-06-04 17:40:02 -0700

    setup .env

--- jolo-app/src/api/supabase.tsx
+++ jolo-app/src/api/supabase.tsx
@@ -1,7 +1,8 @@
 import 'react-native-url-polyfill/auto'
 import * as SecureStore from 'expo-secure-store'
 import { createClient } from '@supabase/supabase-js'
-import Constants from 'expo-constants'
+import {SUPABASE_URL} from '@env'
+import {SUPABASE_ANON_KEY} from '@env'
 
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
@@ -15,8 +16,10 @@ const ExpoSecureStoreAdapter = {
   },
 }
 
-const supabaseUrl = Constants.expoConfig.extra.SUPABASE_URL
-const supabaseAnonKey = Constants.expoConfig.extra.SUPABASE_ANON_KEY
+const supabaseUrl = SUPABASE_URL
+const supabaseAnonKey = SUPABASE_ANON_KEY
+
+console.log(supabaseUrl)
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
