cfb7b305aeb028c4d5da095e0658592008e48638
Khatanbuuvei Bold <khatna@github.com>
2023-07-04 23:42:14 +0800

    USP-35: use generated types

--- jolo-app/src/utils/requests.tsx
+++ jolo-app/src/utils/requests.tsx
@@ -1,9 +1,10 @@
+import { Database } from "../../lib/database.types";
 import { supabase } from "../api/supabase";
-import { PostType, RequestType, UserType } from "../types";
+import { PostType, RequestType } from "../types";
 
 async function getUserDetails() {
   const { data: { user } } = await supabase.auth.getUser();
-  console.log("in getUserDetails()", user)
+  console.log("in getUserDetails()", user);
   return user;
 }
 
@@ -27,7 +28,7 @@ async function insertIntoTable(tableName: string, data: any, user_id: string) {
     .insert([
       {
         user_id: user_id,
-        ...data
+        ...data,
       },
     ]);
 
@@ -49,17 +50,19 @@ export async function insertRequest(params: RequestType) {
     const destinationId = await getLocationId(params.destination);
 
     const time = params.exactTime.toLocaleTimeString();
-    console.log("Time is", params.exactTime.toTimeString().split(' ')[0])
-    console.log("Type is", time)
+    console.log("Time is", params.exactTime.toTimeString().split(" ")[0]);
+    console.log("Type is", time);
 
-    const newData =  { 
+    const newData = {
       departure_location_id: departureId,
       destination_location_id: destinationId,
       description: params.description,
-      ...(params.timeOfDay !== "Цаг оруулах" && { time_of_day: params.timeOfDay }),
+      ...(params.timeOfDay !== "Цаг оруулах" &&
+        { time_of_day: params.timeOfDay }),
       departure_day: params.date,
-      ...(params.timeOfDay === "Цаг оруулах" && { departure_time: params.exactTime.toTimeString().split(' ')[0] })
-    }
+      ...(params.timeOfDay === "Цаг оруулах" &&
+        { departure_time: params.exactTime.toTimeString().split(" ")[0] }),
+    };
     insertIntoTable("requests", newData, user.id);
 
     console.log("inserted ", newData);
@@ -76,7 +79,7 @@ export async function insertPost(params: PostType) {
     const departureId = await getLocationId(params.departure);
     const destinationId = await getLocationId(params.destination);
 
-    const newData =  { 
+    const newData = {
       departure_location_id: departureId,
       destination_location_id: destinationId,
       available_seats: params.availableSeats,
@@ -84,8 +87,9 @@ export async function insertPost(params: PostType) {
       description: params.description,
       time_of_day: params.timeOfDay,
       departure_day: params.date,
-      ...(params.timeOfDay === "Цаг оруулах" && { departure_time: params.exactTime })
-    }
+      ...(params.timeOfDay === "Цаг оруулах" &&
+        { departure_time: params.exactTime }),
+    };
     insertIntoTable("posts", newData, user.id);
 
     console.log("inserted ", newData);
@@ -95,32 +99,33 @@ export async function insertPost(params: PostType) {
   }
 }
 
-export async function fetchData(
-  userType: UserType,
+export async function fetchPosts(
   searchParams?: {
     departure?: string;
     destination?: string;
     date?: Date;
     timeOfDay?: string;
     availableSeats?: number;
-    sortBy?: "date" | "availableSeats";
+    sortBy?: "departure_day" | "available_seats";
   },
-): Promise<any> {
-  const table = userType === "rider" ? "posts" : "requests";
-  const additionalFields = userType === "rider" ? ", fee, available_seats" : "";
+) {
+  type Post = Database["public"]["Tables"]["posts"]["Row"];
+  type Profile = Database["public"]["Tables"]["profiles"]["Row"];
+  type Location = Database["public"]["Tables"]["locations"]["Row"];
 
   let query = supabase
-    .from(table)
+    .from("posts")
     .select(`
       id,
       departure_time,
-      user_id:profiles (first_name),
-      departure_location_id:locations!${table}_departure_location_id_fkey(location_name),
-      destination_location_id:locations!${table}_destination_location_id_fkey(location_name),
+      author: profiles (first_name, avatar_url),
+      departure_location:locations!posts_departure_location_id_fkey (location_name),
+      destination_location:locations!posts_destination_location_id_fkey (location_name),
       departure_day,
       time_of_day,
-      description
-      ${additionalFields}
+      description,
+      fee,
+      available_seats
     `);
 
   if (searchParams) {
@@ -128,18 +133,73 @@ export async function fetchData(
       const departure_id = await getLocationId(searchParams.departure);
       query = query.eq("departure_location_id", departure_id);
     }
-    
+
     if (searchParams.destination) {
       const destination_id = await getLocationId(searchParams.destination);
       query = query.eq("destination_location_id", destination_id);
     }
 
-    // Add more conditions for other search parameters...
-    // if (searchParams.date) {
-    //   query = query.eq("date", searchParams.date);
-    // }
-    if (userType === "rider" && searchParams.availableSeats) {
+    if (searchParams.availableSeats) {
       query = query.gt("available_seats", searchParams.availableSeats);
+    }
+
+    if (searchParams.sortBy) {
+      query = query.order(searchParams.sortBy, { ascending: false });
+    }
+  }
+
+  const { data, error } = await query.returns<
+    (Post & {
+      author: Profile;
+      departure_location: Location;
+      destination_location: Location;
+    })[]
+  >();
+
+  if (error) {
+    console.log("Error: ", error);
+    throw error;
+  }
+
+  return data;
+}
+
+export async function fetchRequests(
+  searchParams?: {
+    departure?: string;
+    destination?: string;
+    date?: Date;
+    timeOfDay?: string;
+    sortBy?: "departure_day";
+  },
+) {
+  type Request = Database["public"]["Tables"]["requests"]["Row"];
+  type Profile = Database["public"]["Tables"]["profiles"]["Row"];
+  type Location = Database["public"]["Tables"]["locations"]["Row"];
+
+  let query = supabase
+    .from("requests")
+    .select(`
+      id,
+      departure_time,
+      author:profiles,
+      author: profiles (first_name, avatar_url),
+      departure_location:locations!posts_departure_location_id_fkey (location_name),
+      destination_location:locations!posts_destination_location_id_fkey (location_name),
+      departure_day,
+      time_of_day,
+      description
+    `);
+
+  if (searchParams) {
+    if (searchParams.departure) {
+      const departure_id = await getLocationId(searchParams.departure);
+      query = query.eq("departure_location_id", departure_id);
+    }
+
+    if (searchParams.destination) {
+      const destination_id = await getLocationId(searchParams.destination);
+      query = query.eq("destination_location_id", destination_id);
     }
 
     // Sort the results if a sortBy option is provided...
@@ -148,38 +208,22 @@ export async function fetchData(
     }
   }
 
-  const { data, error } = await query;
+  const { data, error } = await query.returns<
+    (Request & {
+      author: Profile;
+      departure_location: Location;
+      destination_location: Location;
+    })[]
+  >();
 
   if (error) {
     console.log("Error: ", error);
-    return null;
+    throw error;
   }
 
-  const transformedData = data.map((post: any) => {
-    const baseData = {
-      id: post.id,
-      exactTime: post.departure_time,
-      authorName: post.user_id.first_name,
-      departure: post.departure_location_id.location_name,
-      destination: post.destination_location_id.location_name,
-      description: post.description,
-      date: post.departure_day,
-      timeOfDay: post.time_of_day
-    };
-
-    if (userType === "rider") {
-      return {
-        ...baseData,
-        fee: post.fee,
-        availableSeats: post.available_seats,
-      };
-    }
-
-    return baseData;
-  });
-
-  console.log(JSON.stringify(transformedData, null, 2));
-  console.log(JSON.stringify(data, null, 2));
-
-  return transformedData;
+  return data;
 }
+
+// export return types so that they can be used by components
+export type PostResponse = Awaited<ReturnType<typeof fetchPosts>>;
+export type RequestResponse = Awaited<ReturnType<typeof fetchRequests>>;
