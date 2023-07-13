43bb796c60f6afb2ba719835108e6b7749429ad4
Khatanbuuvei Bold <khatna@github.com>
2023-07-13 23:25:54 +0800

    USP-35: add avatar to post listing

--- jolo-app/src/components/Post.tsx
+++ jolo-app/src/components/Post.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Spacer, Text } from "native-base";
+import { Avatar, Box, HStack, Spacer, Text, VStack } from "native-base";
 import React from "react";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { PostResponse } from "../utils/requests";
@@ -10,19 +10,33 @@ interface PostComponentProps {
 
 const Post: React.FC<PostComponentProps> = ({ post, onClick }) => {
   return (
-    <TouchableOpacity onPress={() => {onClick(post)}}>
+    <TouchableOpacity
+      onPress={() => {
+        onClick(post);
+      }}
+    >
       <Box bg="white" p="3" rounded="lg" my="2" shadow="2">
-        <HStack space={3} alignItems="center">
-          <Text fontWeight="bold" fontSize="md">
-            {post.author.first_name}
-          </Text>
-          <Text>
-            {post.departure_location.location_name} - {post.destination_location.location_name}
-          </Text>
-          <Spacer />
-          <Text>{post.departure_day}</Text>
-          <Text>{post.available_seats}</Text>
-          <Text>{post.fee}</Text>
+        <HStack space={5}>
+          <Avatar
+            size="lg"
+            source={{
+              uri: post.author.avatar_url ||
+                "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
+            }}
+          />
+          <VStack alignItems="left">
+            <Text fontWeight="bold" fontSize="md">
+              {post.author.first_name}
+            </Text>
+            <Text>
+              {post.departure_location.location_name} -{" "}
+              {post.destination_location.location_name}
+            </Text>
+            <Spacer />
+            <Text>{post.departure_day}</Text>
+            <Text>Суудал: {post.available_seats}</Text>
+            <Text>Үнэ: {post.fee}</Text>
+          </VStack>
         </HStack>
       </Box>
     </TouchableOpacity>
